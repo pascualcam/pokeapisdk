3ae8cd9ab164d6a41e4763c17754be4d45e4584a
Pascual Camacho <pascualc02@gmail.com>
2024-08-14 16:44:56 -0700

    update setup

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="pokeapisdk",
-    version="0.1.0",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "requests",
         "click",
         "dataclasses",
-        "unittest",
     ],
     description="A simple collection of classes to interact with the PokeAPI",
     author="Pascual Camacho",
     author_email="me@pascualcam.com",
     url="https://github.com/pascualcam/pokeapisdk/",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
